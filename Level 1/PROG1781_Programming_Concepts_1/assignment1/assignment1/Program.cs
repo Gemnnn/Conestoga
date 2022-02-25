49c508ed4a9fd4e7b2a77b8b26a729f10a596195
Branden Min <brandenmin@gmail.com>
2022-02-25 09:52:12 -0500

    Programming Concept

---
+++ Level 1/PROG1781_Programming_Concepts_1/assignment1/assignment1/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Program ID: Assignment1
+ * 
+ * Purpose: Assignment
+ * 
+ * Revision History:
+ *  created by Byounguk Min Sept 2020
+ *  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameString;
+            string emailString;
+            string experienceString;
+            string countryString;
+            double celsiusDouble;
+            double fahrenheitDouble;
+
+            nameString = "";
+            emailString = "";
+            experienceString = "";
+            countryString = "";
+            celsiusDouble = 0;
+            fahrenheitDouble = 0;
+
+            Console.Write("Please Enter your Name: ");
+            nameString = Console.ReadLine();
+            Console.Write("Please Enter your E-mail: ");
+            emailString = Console.ReadLine();
+            Console.Write("please Enter Cliius: ");
+            celsiusDouble = int.Parse(Console.ReadLine());
+            Console.Write("Please Enter your Experience(Low, Moderate, or High): ");
+            experienceString = Console.ReadLine();
+            Console.Write("Please Enter your Country: ");
+            countryString = Console.ReadLine();
+
+            fahrenheitDouble = (celsiusDouble * 9 / 5) + 32;
+         
+            Console.WriteLine("Your Name Assignment1 Part2");
+
+            Console.WriteLine("Name: " + nameString);
+            Console.WriteLine("Email: " + emailString);
+            Console.WriteLine("The Celsius temp of " + celsiusDouble +
+                " degrees converts to in " + fahrenheitDouble + " Fahrenheit.");
+            Console.WriteLine("Experience: " + experienceString + 
+                " previous programming experience");
+            Console.WriteLine("Country: " + countryString);
+            Console.ReadLine();
+        }
+    }
+}
